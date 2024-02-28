4a73b383c9df713144c55230aa8af359b09db969
akmiii <akmira2410@mail.ru>
2024-02-28 19:26:44 +0600

    checking for prime

--- rsa.py
+++ rsa.py
@@ -4,3 +4,13 @@ def gcd(a, b):
     while b != 0:
         a, b = b, a % b
     return a
+
+def is_prime(num):
+    if num == 2:
+        return True
+    if num < 2 or num % 2 == 0:
+        return False
+    for n in range(3, int(num**0.5)+2, 2):
+        if num % n == 0:
+            return False
+    return True
