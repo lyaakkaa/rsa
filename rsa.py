bc4a2c02834fced4b0a89052383019c4efcbe1b6
21B030917 <98811495+21B030917@users.noreply.github.com>
2024-02-28 21:42:15 +0600

    add multi_inverse

--- rsa.py
+++ rsa.py
@@ -1,4 +1,4 @@
-
+import random
 
 def gcd(a, b):
     while b != 0:
@@ -14,3 +14,29 @@ def is_prime(num):
         if num % n == 0:
             return False
     return True
+
+def multiplicative_inverse(e, phi):
+    d = 0
+    x1 = 0
+    x2 = 1
+    y1 = 1
+    temp_phi = phi
+
+    while e > 0:
+        temp1 = temp_phi // e
+        temp2 = temp_phi - temp1 * e
+        temp_phi = e
+        e = temp2
+
+        x = x2 - temp1 * x1
+        y = d - temp1 * y1
+
+        x2 = x1
+        x1 = x
+        d = y1
+        y1 = y
+
+    if temp_phi == 1:
+        return d + phi
+
+
